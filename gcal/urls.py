a43e79d5e235f7c05b195a0584bc2edc54d1f8ab
Eric Youngson <eayoungs@gmail.com>
2017-11-23 13:05:50 -0800

    linked dynamic page to static landing page with namespaced url in template at index

--- gcal/urls.py
+++ gcal/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
+
 from . import views
 
 
+app_name = 'gcal'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^events_list/', views.events_list, name='')
+    url(r'^events_list/$', views.events_list, name='events_list')
 ]
--- gcal/views.py
+++ gcal/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.contrib.auth.models import User
 
 from .models import CalEvent
 
