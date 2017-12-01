1c8c7ce4c78e167a14c1c688c6650a24dba0c25f
Eric Youngson <eayoungs@gmail.com>
2017-11-30 23:54:22 -0800

    Added text to index view

--- gcal/views.py
+++ gcal/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.views.generic import TemplateView
-from django.contrib.auth.models import User
+#from django.contrib.auth.models import User
+from django.shortcuts import render
 
 from .models import CalEvent
 
@@ -15,8 +16,20 @@ class ContactView(TemplateView):
 
 
 def index(request):
-    template = loader.get_template("gcal/index.html")
-    return HttpResponse(template.render())
+    return render(request, "gcal/index.html", {
+        'quoteText': "Everything",
+        'quoteAttrib': '"This Moment is All there Is" --Rumi',
+        'subheading1': "Build your routine, your way",
+        'subtext1': ''' Find out where your spend your time, 
+                    setgoals and check in on your progress 
+                    with minimal effort--all in from within 
+                    the calendar you already know & love: 
+                    Google Calendar ''',
+        'subheading2': "Show the muse you're serious",
+        'subtext2': ''' You have to show up for your dreams; no 
+                    one can do it for you but we can give 
+                    you the tools to stay present '''
+        })
 
 
 def events_list(request):
