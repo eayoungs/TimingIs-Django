173545c7d21fc0d9251ce66e39f780f2b06e6850
Eric Youngson <eayoungs@gmail.com>
2017-12-04 18:27:50 -0800

    Fixed url routes for nav

--- gcal/urls.py
+++ gcal/urls.py
@@ -7,8 +7,8 @@ from . import views
 app_name = 'gcal'
 urlpatterns = [
     url(r'^$', views.view_home, name='index'),
-    url(r'^about/$', views.view_home, name='about'),
-    url(r'^contact/$', views.view_about, name='contact'),
+    url(r'^about/$', views.view_about, name='about'),
+    url(r'^contact/$', views.view_contact, name='contact'),
     url(r'^auth_user/$', views.view_google_api_user_auth, name='auth_user'),
     url(r'^callback/$', views.view_google_api_callback, name='callback'),
     url(r'^events_list/$', views.events_list, name='events_list')
